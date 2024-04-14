db4c9f379d8ea6dab97b4108a7e399c8684f833e
paramkr2 <param71kr@gmail.com>
2024-04-14 09:54:38 +0530

    assets folder included

--- src/server.ts
+++ src/server.ts
@@ -1,17 +1,11 @@
-import app from './index';
-import dbconnect from './db/db';
-
+import app from './index'
+import dbconnect from './db/db'
 const port = 8000;
 
-async function startServer() {
-  try {
-    await dbconnect();
-    app.listen(port, () => {
-      console.log(`Server running at ${port}`);
-    });
-  } catch (error) {
-    console.error('Error connecting to the database:', error);
-  }
-}
+dbconnect();
 
-startServer();
+app.listen( port , ()=>{
+	console.log(`server running at ${port}`)
+});
+
+export default app ;
